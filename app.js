12a8ee2c14de5809aae9861b1d9f924c51d24d8a
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2024-10-12 21:56:22 +0300

    Create index route and view to display initial messages

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const PORT = 3000;
 const app = express();
 
+const indexRouter = require('./routes/indexRouter');
+
+app.use('/', indexRouter);
+
+app.set('view engine', 'ejs');
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
