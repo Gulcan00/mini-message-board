4b8c948c47520258a71d14d47de9b804c3c2e69a
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2024-10-19 16:18:46 +0300

    Create get, post routes for new message form. Create form view.

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 
 const indexRouter = require('./routes/indexRouter');
 
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/', indexRouter);
 
 app.set('view engine', 'ejs');
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -18,4 +18,14 @@ indexRouter.get('/', (req, res) => {
   res.render('index', { title: 'Mini Messageboard', messages });
 });
 
+indexRouter.get('/new', (req, res) => {
+  res.render('form', { title: 'New Message' });
+});
+
+indexRouter.post('/new', (req, res) => {
+  const { messageText, messageUser } = req.body;
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect('/');
+});
+
 module.exports = indexRouter;
